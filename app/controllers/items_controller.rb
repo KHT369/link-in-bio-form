d529851f126b2b27d34596bf7c4087ef68e4552c
KHT369 <ktsao@chicagobooth.edu>
2024-11-09 16:08:31 +0000

    save 1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,4 +4,25 @@ class ItemsController < ApplicationController
 
     render({ :template => "item_templates/list" })
   end
+
+  def backdoor
+
+    render({ :template => "item_templates/backdoor"})
+  end
+
+  def index_save
+    @query = params.fetch("query_link_url")
+    @query_name = params.fetch("query_link_description")
+    @image = params.fetch("query_thumbnail_url")
+    
+    x = Item.new
+    x.link_url = @query
+    x.link_description = @query_name
+    x.thumbnail_url = @image
+    x.save
+
+    #render({ :template => "item_templates/list" })
+
+    redirect_to("/")
+  end
 end
